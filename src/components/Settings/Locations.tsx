09e4a36600fd32d8c6815fd59b929195e4e0b709
David Rose <doppler@gmail.com>
2020-01-15 14:38:36 -0500

    stub out Locations.handleEditLocationLink

--- src/components/Settings/Locations.tsx
+++ src/components/Settings/Locations.tsx
@@ -1,19 +1,23 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
 import { MdAddBox, MdEdit } from "react-icons/md";
 
 type LocationProps = {
   code: string;
   name: string;
+  handleEditLocationClick: (code: string) => void;
 };
 
-const Location = ({ code, name }: LocationProps) => {
+const Location = ({ code, name, handleEditLocationClick }: LocationProps) => {
   return (
     <>
       <div>{name}</div>
       <div>{code}</div>
       <div>
-        <button className="icon">
+        <button
+          className="icon"
+          onClick={() => handleEditLocationClick(code)}
+          id={code}
+        >
           <MdEdit />
         </button>
       </div>
@@ -37,7 +41,13 @@ const initialState = [
 ];
 
 const Locations: React.FC = () => {
-  const [locations, setLocations] = useState(initialState);
+  // @ts-ignore
+  const [locations, setLocations] = useState(initialState); // eslint-disable-line @typescript-eslint/no-unused-vars
+
+  const handleEditLocationClick = (code: string) => {
+    console.log(code);
+  };
+
   return (
     <div className="Locations panel">
       <div className="panel-header">
@@ -49,7 +59,12 @@ const Locations: React.FC = () => {
       <div className="panel-body">
         <div className="location-grid grid">
           {locations.map(location => (
-            <Location code={location.code} name={location.name} />
+            <Location
+              key={location.code}
+              code={location.code}
+              name={location.name}
+              handleEditLocationClick={handleEditLocationClick}
+            />
           ))}
         </div>
       </div>
