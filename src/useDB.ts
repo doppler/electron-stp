dd65491a0efc3d20b0ac0b523d567c6a888f1e6d
David Rose <doppler@gmail.com>
2020-01-24 15:36:35 -0500

    delete user id saving _delete'ed instructor

--- src/useDB.ts
+++ src/useDB.ts
@@ -1,13 +1,20 @@
 import { useContext, useCallback } from 'react';
 import DBContext from './components/DBContext';
+import useAuth from './components/Auth/useAuth';
 
 const useDB = () => {
   const DB = useContext(DBContext);
+  const { usersDB } = useAuth();
 
   const put = useCallback(
     async doc => {
       let result;
       try {
+        if (doc._deleted && doc.type === 'instructor') {
+          usersDB
+            .get(`org.couchdb.user:${doc.email}`)
+            .then(user => usersDB.remove(user));
+        }
         // @ts-ignore
         result = await DB.put(doc);
       } catch (error) {
@@ -15,7 +22,7 @@ const useDB = () => {
       }
       return result;
     },
-    [DB]
+    [DB, usersDB]
   );
 
   const get = useCallback(
