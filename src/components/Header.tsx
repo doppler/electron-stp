fedf2ae2f4e36d03de9d8b1fd09d7bf906415d48
David Rose <doppler@gmail.com>
2020-02-01 21:38:34 -0500

    Header -> styled

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,46 +1,75 @@
-import './Header.css';
 import { MdSettings, MdLock, MdHome } from 'react-icons/md';
 import { IconContext } from 'react-icons';
 import React from 'react';
 import useAuth from './Auth/useAuth';
 import { Link, NavLink } from 'react-router-dom';
+import styled from 'styled-components';
 
+const StyledHeader = styled.div`
+  height: 2em;
+  width: 100%;
+  border-bottom: var(--panel-border);
+  display: flex;
+  align-items: center;
+  & div {
+    flex-grow: 1;
+    display: flex;
+  }
+  & .left {
+    justify-content: flex-start;
+    & > * {
+      margin-left: 0.5em;
+    }
+  }
+  & .right {
+    justify-content: flex-end;
+    & > * {
+      margin-right: 0.5em;
+    }
+  }
+  & span.breadcrumbs {
+    white-space: pre;
+    overflow-x: hidden;
+    & > span::after {
+      content: ' > ';
+    }
+    & > span:last-child::after {
+      content: '';
+    }
+  }
+`;
 const Header = () => {
-  // const location = useLocation();
-
   const { user, isAdminUser } = useAuth();
 
   return (
     <IconContext.Provider value={{ className: 'react-icons' }}>
-      <div className="Header">
-        <div className="left">
-          <NavLink exact to="/" className="link">
+      <StyledHeader>
+        <div className='left'>
+          <NavLink exact to='/' className='link'>
             <MdHome />
           </NavLink>
-          {/*
-          <span className="breadcrumbs">
+          {/* <span className='breadcrumbs'>
             {location.pathname
               .replace(/\//, '')
               .split('/')
               .map((name, i) => (
                 <span key={i}>{name}</span>
               ))}
-          </span>
-              */}
+          </span> */}
         </div>
-        <div className="right">
+        <div className='right'>
           <span>{user.name}</span>
           {/* <span>{JSON.stringify(user.roles)}</span> */}
-          <Link to="/logout" className="link">
+          <Link to='/logout' className='link'>
             <MdLock />
           </Link>
           {isAdminUser() && (
-            <NavLink to="/settings" className="link">
+            <NavLink to='/settings' className='link'>
               <MdSettings />
             </NavLink>
           )}
         </div>
-      </div>
+      </StyledHeader>
     </IconContext.Provider>
   );
 };
